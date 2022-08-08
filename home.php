7ad3804c8fcb87977a66f0a2ede8e4c5781d0d0b
biaaPrado <104280744+biaaPrado@users.noreply.github.com>
2022-08-08 08:47:00 -0300

    Inserindo Home
    Inserindo códigos Home - Bia e deletando códigos teste - André

---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/60a756ccae.js" crossorigin="anonymous"></script>
+    <link rel=stylesheet type="text/css" href="home.css">
+    <title>KeyFriends</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="menu">
+                <input type="checkbox" id="check">
+                <label for="check"> <img src="iconMenu.png" id="iconMenu"> </label>
+                
+                    <ul>
+                        <li> <a href="#"> HOME </a> </li>
+                        <li> <a href="#"> PRODUTOS </a> </li>
+                        <li> <a href="#"> CADASTRAR </a> </li>
+                        <li> <a href="#"> SOBRE </a> </li>
+                    </ul>
+            </div>
+
+            <div class="navbar">
+                <div class="flexbox">
+                    <div class="search">
+                        <!-- <h1>Click para pesquisar</h1> -->
+                        <div>
+                            <input type="text" placeholder="Search..." required>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="icons">
+                    <i id="icon" class="fa-solid fa-gear fa-3x"></i>
+                    <i id="icon" class="fa-solid fa-cart-shopping fa-3x"></i>
+                    <i id="icon" class="fa-solid fa-circle-user fa-3x"></i>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <!-- <section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint totam dolorem veritatis sed distinctio quaerat animi repudiandae quas est. Eaque corrupti quos dolor, similique error aspernatur tenetur? Doloremque, est explicabo?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur cum earum magni eligendi quibusdam vero ducimus impedit, quas explicabo ea, adipisci quidem dolorem voluptas a iusto nostrum quos doloremque id!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, cumque fugiat. Eum delectus cum eveniet adipisci saepe possimus voluptatibus. Reiciendis quo itaque perferendis odio quis sint rerum, perspiciatis ratione dicta.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo commodi officiis tempore iusto, eveniet veritatis dolorem beatae reiciendis, minima earum quas a harum! Labore ducimus neque sit ex. Tempora, id?
+    </section> -->
+
+    <footer>
+
+    </footer>
+</body>
+</html>
