59379cae9498beb92d6d5ef8f5736f31b370467a
André Betetto <andrebetetto165@gmail.com>
2022-08-15 11:54:58 -0300

    Create perfil.php

---
+++ perfil.php
@@ -0,0 +1,79 @@
+<?php
+    session_start();
+
+
+    //upload
+    if(isset($_POST['salvar']) && $_POST['salvar'] == 'Salvar') {
+        $upload_folder = getcw().DIRECTORY_SEPARATOR.'/uploads/';
+        
+
+
+    }
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/60a756ccae.js" crossorigin="anonymous"></script>
+    <link rel=stylesheet type="text/css" href="home.css">
+    <title>HOME | KeyFriends</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="menu">
+                <input type="checkbox" id="check">
+                <label for="check" id="icone"><img src="iconeMenu.png"/></label>
+                <div class="barra">	
+                    <nav class="links">
+                        <a href="home.php"><div class="link">Home</div></a>
+                        <a href=""><div class="link">Produtos</div></a>
+                        <a href=""><div class="link">Cadastrar</div></a>
+                        <a href=""><div class="link">Contato</div></a>
+                    </nav>	
+                </div>
+            </div>
+
+            <div class="navbar">
+                <div class="flexbox">
+                    <div class="search">
+                        <!-- <h1>Click para pesquisar</h1> -->
+                        <div>
+                            <input type="text" placeholder="Pesquisar..." required>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="icons">
+                    <a href="#config"> <i id="icon" class="fa-solid fa-gear fa-2x"> </i> </a>
+                    <a href="#carrinho"> <i id="icon" class="fa-solid fa-cart-shopping fa-2x"> </i> </a>
+                    <a href="#perfil"> <i id="icon" class="fa-solid fa-circle-user fa-2x"> </i> </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <?php
+    $nome = $_SESSION['name'];
+    ?>
+
+    <form action="" method="POST">
+        <label>Coloque aqui uma imagem: <input type="file" name="image"></label>
+        <input type="submit" name="salvar" valor="Salvar">
+    </form>
+
+    <!-- <section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint totam dolorem veritatis sed distinctio quaerat animi repudiandae quas est. Eaque corrupti quos dolor, similique error aspernatur tenetur? Doloremque, est explicabo?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur cum earum magni eligendi quibusdam vero ducimus impedit, quas explicabo ea, adipisci quidem dolorem voluptas a iusto nostrum quos doloremque id!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, cumque fugiat. Eum delectus cum eveniet adipisci saepe possimus voluptatibus. Reiciendis quo itaque perferendis odio quis sint rerum, perspiciatis ratione dicta.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo commodi officiis tempore iusto, eveniet veritatis dolorem beatae reiciendis, minima earum quas a harum! Labore ducimus neque sit ex. Tempora, id?
+    </section> -->
+
+    <footer>
+
+    </footer>
+</body>
+</html>
