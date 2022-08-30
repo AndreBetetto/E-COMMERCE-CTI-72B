22c6183d8a967eb2650612058197801fe11c972e
biaaPrado <104280744+biaaPrado@users.noreply.github.com>
2022-08-30 07:31:11 -0300

    uso de placeholder

--- home.php
+++ home.php
@@ -15,7 +15,7 @@
     <?php 
         include('navbar.php')
     ?>
-    <img id="capa" src="./imagens/image.png" alt="meramente ilustrativa">
+    <img id="capa" src="https://via.placeholder.com/1520x300" alt="meramente ilustrativa">
     <footer>
 
     </footer>
