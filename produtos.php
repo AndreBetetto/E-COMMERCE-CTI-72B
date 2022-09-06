dfb3a36f1c5043f361bcae5bf07565582553f6fc
biaaPrado <104280744+biaaPrado@users.noreply.github.com>
2022-09-06 09:27:57 -0300

    coisas bonitas

--- produtos.php
+++ produtos.php
@@ -36,29 +36,26 @@
             return;
         }
             echo "<div class='grid'>";
+                foreach($resultado_lista as $linha)
+                {
+                    $precoProd = NUmber_format($linha['preco'], 2, ',','.');
+                    echo "<div class='itens'> 
+                        <a href='selecao_detalhes_front.php?id=".$linha['id']."'>
+                            <img src='https://via.placeholder.com/250'/>
+                        </a>
 
-            foreach($resultado_lista as $linha)
-            {
-                $precoProd = NUmber_format($linha['preco'], 2, ',','.');
-                echo "<div class='grid'> 
-                    <a href='selecao_detalhes_front.php?id=".$linha['id']."'>
-                        <img src='https://via.placeholder.com/250'/>
-                    </a>
-                </div>";
+                        <div> <p>".$linha['descricao']."</p> </div>
+                        <div> <p> R$ ".$precoProd."</div>";
 
-                echo "<div> 
-                    <div> <p>".$linha['descricao']."</p> </div>
-                    <div> <p> R$ ".$precoProd."</div>";
-
-                    if($linha['estoque']<=0){
-                        echo "<div> <span> Produto esgostado</span></div>";
-                    }
-                    else{
-                        echo "<div> <span>".$linha['estoque']." em estoque </span></div>";
-                    }
-                    echo "<a href='addprodcar.php?id=add%codproduto=".$linha['id']."'>Comprar</a>";
-                echo "</div>";
-            }
+                        if($linha['estoque']<=0){
+                            echo "<div> <span> Produto esgostado</span></div>";
+                        }
+                        else{
+                            echo "<div> <span>".$linha['estoque']." em estoque </span></div>";
+                        }
+                        echo "<a href='addprodcar.php?id=add%codproduto=".$linha['id']."'>Comprar</a>";
+                    echo "</div>";
+                }
             echo "</div>";
 
     ?>
