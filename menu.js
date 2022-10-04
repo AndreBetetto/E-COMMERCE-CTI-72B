774bdcb8b00d05606f9d94a15d47a60c67be05d4
biaaPrado <104280744+biaaPrado@users.noreply.github.com>
2022-10-04 12:20:33 -0300

    aa

--- menu.js
+++ menu.js
@@ -5,3 +5,11 @@ function abrirMenu(){
 function fecharMenu(){
     document.getElementById('menu').style.width = "0px";
 }
+
+document.querySelector('.menu-trigger').onclick = () => {
+    document.querySelector('.menu-menu').style.display == 'none' ? 'block' : 'none';
+}
+
+function abreMenuzinho(){
+    document.getElementById('menu-menu').style.display = "block";
+}
