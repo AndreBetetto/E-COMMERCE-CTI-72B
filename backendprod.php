c978d17701aa8f28234516ed35e9f055e82be419
André Betetto <andre.betetto@unesp.br>
2022-10-06 12:04:22 -0300

    update

--- backendprod.php
+++ backendprod.php
@@ -21,13 +21,12 @@ if($_SESSION['buscabool'] == false) {
     pg_close($conexao);
     
 } else {
-
     $contProd = "SELECT COUNT(*) as total FROM produtosandre where CONCAT(id, titulo, material) LIKE 'andre'";
     $rowProd = pg_fetch_row(pg_query($conexao, $contProd));
     //$sql="SELECT * FROM produtosandre where CONCAT(id, titulo, material) LIKE '%$buscaProd%' ORDER BY id";
-    $sql="SELECT * FROM produtosandre where CONCAT(id, titulo, material) LIKE 'andre' ORDER BY id";
+    $sql="SELECT * FROM produtosandre where CONCAT(id, titulo, material) LIKE '%andre%' ORDER BY id";
 
-    $resultado= pg_query($conexao, $sql);
+    $resultado = pg_query($conexao, $sql);
     $qtde=pg_num_rows($resultado);
     $resultado_lista = null;
     if ($qtde > 0)
