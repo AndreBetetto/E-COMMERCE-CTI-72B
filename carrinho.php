bae3341a393a3690cb8e758e259a30b24437b741
André Betetto <andre.betetto@unesp.br>
2022-10-09 01:49:38 -0300

    Update carrinho.php

--- carrinho.php
+++ carrinho.php
@@ -31,22 +31,61 @@
     $contagem = pg_fetch_row(pg_query($conexao, $cont));
     ?>
     <div class="titleDB"> <p>Meu carrinho<p> </div>
-    <main class="carrinho">
-        <section class="prod">
-            <div class="itens">
-                <div class="imgTitulo">
-                    <img src="imagens/bia.jpg">
-                    <div class="titulo"> 
-                        <label id="title">Chaveiro</label>
+    <?php 
+    for($i = 0; $i < $contagem[0]; $i++)
+    {
+        $sqlCarrinho = "select * from carrinhoandre where id_user = $id order by id_produto";
+        $queryCarrinho = pg_query($conexao, $sqlCarrinho);
+        $carrinho = pg_fetch_assoc($queryCarrinho, $i);
+        $carrinhoID = intval($carrinho['id_produto']);
+
+        $contador = "select count(*) from carrinhoandre where id_produto = $carrinhoID";
+        $numcont = pg_fetch_row(pg_query($conexao, $contador));
+        if($numcont == 0) {
+            echo "Nenhum produto no carrinho";
+        }
+        $sqlpega = "select * from produtosandre where id = $carrinhoID order by id";
+        $sqlmostra = pg_fetch_assoc(pg_query($conexao, $sqlpega));
+    
+        $precoTot = $sqlmostra['preco']* $carrinho['qtd'];
+
+        $caminho = $sqlmostra['id'].  $sqlmostra['numberphoto'].'.jpg'; 
+        $caminho2 = $sqlmostra['id']. $sqlmostra['numberphoto'].'.png';
+        $caminho3 = $sqlmostra['id']. $sqlmostra['numberphoto'].'.jpeg';
+
+        $target  = "produtosimagem/" . $caminho;
+        $target2 = "produtosimagem/" . $caminho2;
+        $target3 = "produtosimagem/" . $caminho3;
+
+        if(file_exists($target)) {
+            $img = "<img src='$target' width='250' height='250'/>";
+        } elseif(file_exists($target2)){
+             $img = "<img src='$target2' width='250' height='250'/>";
+        } elseif(file_exists($target3)){
+            $img = "<img src='$target3' width='250' height='250'/>";
+        } else {
+            $img = "<img src='produtosimagem/default.png' width='250' height='250'/>";
+        } 
+
+        
+
+    echo "
+    <main class='carrinho'>
+        <section class='prod'>
+            <div class='itens'>
+                <div class='imgTitulo'>". $img ."
+                    
+                    <div class='titulo'> 
+                        <label id='title'>". $sqlmostra['titulo'] ."</label>
                         <div class='alter'>
                                 <form method='post' action='menosprodcar.php'>
-                                    <button class='alterProds' type='submit' name='submit' id='1' value ='1'> 
+                                    <button class='alterProds' type='submit' name='submit' id='".$carrinhoID."-submit' value ='".$carrinhoID."'> 
                                         <i class='fa-solid fa-minus'></i>
                                     </button>
                                 </form>
-                                <label class="qtde"> 5 </label>
+                                <label class='qtde'>". $carrinho['qtd'] ."  </label>
                                 <form method='post' action='maisprodcar.php'> 
-                                    <button class='alterProds' type='submit' name='submit' id='1' value ='1'> 
+                                    <button class='alterProds' type='submit' name='submit' id='".$carrinhoID."-submit' value ='".$carrinhoID."'> 
                                         <i class='fa-solid fa-plus'></i>
                                     </button>
                                 </form> 
@@ -54,27 +93,55 @@
                     </div>
                 </div>
 
-                <div class="preco">
+                <div class='preco'>
                     <label>Preço unitário</label>
-                    <div class="val">
-                        <label> R$ 1400,00</label>
+                    <div class='val'>
+                        <label> R$ ". Number_format($sqlmostra['preco'] * $carrinho['qtd'], 2, ',','.') ."</label>
                     </div>
                 </div>
             </div>
             
-        </section>
+        </section>";
+    } 
+        ?>
         
-        <section class="list">
+    
+        <section class='list'>
             <div class="resumo">
                 <span class="subtitulo">Resumo da compra</span>
                 <div class="menuResumo"> 
                     <div class="menuItens">
-                        <div class="menuConteudo">
-                            <span>Chaveiro 1</span>
-                            <span>R$ 3,40</span>
-                        </div>
+                    <?php
+                    $cont = "select count(*) from carrinhoandre where id_user = $id";
+                    $contagem = pg_fetch_row(pg_query($conexao, $cont));    
+                    
 
-                        <div class="menuConteudo">
+                    for($i = 0; $i < $contagem[0]; $i++)
+                    {
+                        $sqlCarrinho = "select * from carrinhoandre where id_user = $id order by id_produto";
+        $queryCarrinho = pg_query($conexao, $sqlCarrinho);
+        $carrinho = pg_fetch_assoc($queryCarrinho, $i);
+        $carrinhoID = intval($carrinho['id_produto']);
+
+        $contador = "select count(*) from carrinhoandre where id_produto = $carrinhoID";
+        $numcont = pg_fetch_row(pg_query($conexao, $contador));
+        if($numcont == 0) {
+            echo "Nenhum produto no carrinho";
+        }
+        $sqlpega = "select * from produtosandre where id = $carrinhoID order by id";
+        $sqlmostra = pg_fetch_assoc(pg_query($conexao, $sqlpega));
+        $valortotal = $valortotal + ($sqlmostra['preco'] * $carrinho['qtd']);
+                        echo "
+                        <div class='menuConteudo'>
+                            <span>".  $sqlmostra['titulo'].", ".$carrinho['qtd']." quantidadess."."</span>
+                            <span> R$ ". Number_format($sqlmostra['preco'] * $carrinho['qtd'], 2, ',','.')  ."</span>
+                        </div>
+                        </div>";
+                    }
+
+                        
+
+                        /*<div class="menuConteudo">
                             <span>Chaveiro 2</span>
                             <span>R$ 6,40</span>
                         </div>
@@ -92,18 +159,20 @@
                         <div class="menuConteudo">
                             <span>Chaveiro 1</span>
                             <span>R$ 3,40</span>
-                        </div>
-                    </div>
-
-                    <div class="menuTotal"> 
+                        </div>*/
+                    
+                    /*
+                    
+                    */
+                    echo "<div class='menuTotal'> 
                         <span>Total</span>
-                        <span>R$ 9,80</span>
+                        <span>R$ ".  $valortotal * $carrinho['qtd']. "</span>
                     </div>
                 </div>
-                    
-                <a class="addCmp" href='addprodcar.php?id=<?php echo $idProd?>'> Finalizar compra </a>  
+                <a class='addCmp' href='addprodcar.php?id=".$idProd."'> Finalizar compra </a>  
             </div>
-        </section>
+        </section>";
+        ?>
     </main>
 
         <br> <br> <br> <br> 
