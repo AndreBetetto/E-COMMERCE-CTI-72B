e08affef069bd56952282f2a1d87ea27308d3758
André Betetto <andre.betetto@unesp.br>
2022-10-09 15:38:30 -0300

    update

---
+++ finalizacaocompra_1.php
@@ -0,0 +1,12 @@
+<?php
+    include ('conexao.php');
+    $id = $_SESSION['id'];
+
+    
+
+
+
+
+
+?>
+
---
+++ naoapagaressearquivo.sql
@@ -0,0 +1,9 @@
+create table pedidosandre {
+	id bigserial not null primary key,
+	id_user int
+}
+
+create table vendasandre (
+	id bigserial not null primary key,
+	id_prod 
+)
